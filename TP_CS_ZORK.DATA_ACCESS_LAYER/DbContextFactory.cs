7d82ac86bd2935baf87d6fea29f15bc1b5eaa51a
Corentin Dupont <dupont.corentin44@gmail.com>
2021-02-12 14:52:49 +0100

    ✨ feat(db context): create db context factory

---
+++ TP_CS_ZORK.DATA_ACCESS_LAYER/DbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP_CS_ZORK.DATA_ACCESS_LAYER.Models;
+
+namespace TP_CS_ZORK.DATA_ACCESS_LAYER
+{
+    class DbContextFactory : IDesignTimeDbContextFactory<ZorkDbContext>
+    {
+        public ZorkDbContext CreateDbContext(string[] args)
+        {
+            var dbContextBuilder = new DbContextOptionsBuilder<ZorkDbContext>();
+            dbContextBuilder.UseSqlServer("Server=localhost;Database=ZorkDb;Trusted_Connection=True;", opt => opt.MigrationsAssembly("EntityFrameworkDbFirst"));
+
+            return new (dbContextBuilder.Options);
+        }
+    }
+}
