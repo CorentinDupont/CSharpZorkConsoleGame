36f77fec01a388ae4c2441afbddc76ee6b9b7cd5
Lerenard <lerenard.charly@gmail.com>
2021-02-12 09:04:49 +0100

    init commit

---
+++ TP_CS_ZORK.CONSOLE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP_CS_ZORK.CONSOLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
