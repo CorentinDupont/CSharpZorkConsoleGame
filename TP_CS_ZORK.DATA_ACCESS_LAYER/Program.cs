c4e663994f9f8ae74cee2da4609da14200caf61e
Corentin Dupont <dupont.corentin44@gmail.com>
2021-02-12 13:22:43 +0100

    📦 conf(init models): install entity framework libs

---
+++ TP_CS_ZORK.DATA_ACCESS_LAYER/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP_CS_ZORK.DATA_ACCESS_LAYER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
