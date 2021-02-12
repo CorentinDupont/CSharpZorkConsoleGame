637cd0b7155349fd867166d4d5e72d325df7b036
Lerenard <lerenard.charly@gmail.com>
2021-02-12 15:11:44 +0100

    feat(menus): about & refacto main

--- TP_CS_ZORK.CONSOLE/Program.cs
+++ TP_CS_ZORK.CONSOLE/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using TP_CS_ZORK.CONSOLE.commands;
+using TP_CS_ZORK.CONSOLE.commandsMenu;
 
 namespace TP_CS_ZORK.CONSOLE
 {
@@ -7,53 +7,8 @@ namespace TP_CS_ZORK.CONSOLE
     {
         static void Main(string[] args)
         {
-            ICommand[] commands = new ICommand[]
-            {
-                new CmdAbout(),
-                new CmdCreateNewGame(),
-                new CmdExit(),
-                new CmdLoadSavedGame()
-            };
-
-            Console.WriteLine("\n\n\n");
-
-            Console.WriteLine("Enter your name!");
-            Console.WriteLine("\n");
-            string userName = Console.ReadLine();
-
-            Console.WriteLine("\n\n\n");
-
-            // This loop creates a list of commands:
-            displayCommands(commands);
-
-                        // Read until the input is valid.
-                var userChoice = string.Empty;
-                var commandIndex = -1;
-
-                while (!int.TryParse(userChoice, out commandIndex) || commandIndex > commands.Length || userChoice == "0")
-                {
-                    Console.Clear();
-                    Console.WriteLine($"Select number between 0 and {commands.Length} \n");
-
-                    displayCommands(commands);
-
-                    userChoice = Console.ReadLine();
-                }
-                
-                Console.WriteLine($"commandIndex {commandIndex}");
-                commands[commandIndex - 1].Execute(commandIndex);
-
-            Console.WriteLine("\n\n\n");
-        }
-
-        public static void displayCommands(ICommand[] commands)
-        {
-            for (int i = 0; i < commands.Length; i++)
-            {
-                Console.WriteLine("{0}. {1}", i + 1, commands[i].Description);
-            }
-            Console.WriteLine("\n");
-
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.display();
         }
     }
 }
--- TP_CS_ZORK.CONSOLE/commandsMenu/CmdCreateNewGame.cs
+++ TP_CS_ZORK.CONSOLE/commandsMenu/CmdCreateNewGame.cs
@@ -11,6 +11,12 @@ namespace TP_CS_ZORK.CONSOLE.commands
         public string Description => "Create new game";
 
         public void Execute(int number)
-        {}
+        {
+            Console.WriteLine("Enter your name!");
+            Console.WriteLine("\n");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("\n\n\n");
+        }
     }
 }
