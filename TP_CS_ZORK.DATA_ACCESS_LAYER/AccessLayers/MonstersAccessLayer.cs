93856176c70366d79e45d10d3f7280310a62a67e
Corentin Dupont <dupont.corentin44@gmail.com>
2021-02-21 20:15:19 +0100

    🚧 feat(data access layer): add base access layer and create monster's one

---
+++ TP_CS_ZORK.DATA_ACCESS_LAYER/AccessLayers/MonstersAccessLayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP_CS_ZORK.DATA_ACCESS_LAYER.Models;
+
+namespace TP_CS_ZORK.DATA_ACCESS_LAYER.AccessLayers
+{
+    class MonstersAccessLayer : BaseAccessLayer<Monster>
+    {
+
+        public MonstersAccessLayer(ZorkDbContext context) : base(context) {}
+    }
+}
