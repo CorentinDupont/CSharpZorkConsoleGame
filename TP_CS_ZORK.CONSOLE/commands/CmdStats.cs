cc26090fb2ba5d2cd69d4d63c981743ad2b690a6
Corentin Dupont <dupont.corentin44@gmail.com>
2021-02-25 22:41:26 +0100

    ✨ feat(stats): display player position

--- TP_CS_ZORK.CONSOLE/commands/CmdStats.cs
+++ TP_CS_ZORK.CONSOLE/commands/CmdStats.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using TP_CS_ZORK.CONSOLE.commands;
 using TP_CS_ZORK.CONSOLE.utils;
+using TP_CS_ZORK.DATA_ACCESS_LAYER.AccessLayers;
 
 namespace TP_CS_ZORK.CONSOLE.commands
 {
@@ -15,10 +16,11 @@ namespace TP_CS_ZORK.CONSOLE.commands
         public async Task ExecuteAsync(int number)
         {
             var player = await GameInstance.GetPlayerInstance();
+            var currentCell = player.Cells.Single(c => c.PlayerPresence == true);
 
             Console.WriteLine("Stats: ");
             Console.WriteLine($"HP: {player.Hp}/{player.MaxHp}");
-            Console.WriteLine($"Current coordinates: x:{player.CurrentCell.PosX} y:{player.CurrentCell.PosY}");
+            Console.WriteLine($"Current coordinates: x:{currentCell.PosX} y:{currentCell.PosY}");
 
             Console.ReadLine();
 
