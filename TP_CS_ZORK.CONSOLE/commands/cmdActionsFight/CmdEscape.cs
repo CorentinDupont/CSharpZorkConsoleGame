7ab6ef8960a2f07834fff58aab2424ed22621eef
Corentin Dupont <dupont.corentin44@gmail.com>
2021-02-25 23:19:59 +0100

    ✨ feat(escape): escape command

--- TP_CS_ZORK.CONSOLE/commands/cmdActionsFight/CmdEscape.cs
+++ TP_CS_ZORK.CONSOLE/commands/cmdActionsFight/CmdEscape.cs
@@ -8,13 +8,42 @@ using TP_CS_ZORK.CONSOLE.utils;
 
 namespace TP_CS_ZORK.CONSOLE.commands
 {
-    class CmdEscape : ICommand
+    class CmdEscape : ICommandAsync
     {
         public new string Description => "Escape";
 
-        public void Execute(int number)
+        public async Task ExecuteAsync(int number)
         {
-           
+            var player = await GameInstance.GetPlayerInstance();
+            var currentCell = player.Cells.Single(c => c.PlayerPresence == true);
+            var monster = GameInstance.GetFightingMonster();
+            Random random = new Random();
+            var escapeChance = random.Next(0, 100);
+
+            if (escapeChance < monster.MissRate)
+            {
+                Console.WriteLine("You managed to escape the monster!");
+
+                var previousMenu = new Menu(
+                    CommandsEnum.CmdInventory.ToString(),
+                    CommandsEnum.CmdStats.ToString(),
+                    CommandsEnum.CmdMove.ToString(),
+                    CommandsEnum.CmdExit.ToString()
+                );
+
+                Menu menu = new Menu(
+                    new CmdMoveNorth(),
+                    new CmdMoveEst(),
+                    new CmdMoveSouth(),
+                    new CmdMoveWest(),
+                    new CmdGoBack(previousMenu)
+                );
+
+                await menu.Activate();
+            } else
+            {
+                GameInstance.Fight(monster, player);
+            }
         }
     }
 }
